d3294e6d271ad70f94e56aec23e836986708e2d4
hackingyseguridad <antonio_taboada@hotmail.es>
2026-04-16 16:45:08 +0200

    Create scanbpfdoor.sh

---
+++ scanbpfdoor.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# hakingyseguridad.com
+# Script SIMPLE para detectar BPFdoor via ICMP
+# Busca respuesta al paquete mágico 0xFFFFFFFF
+
+FICHERO="ip.txt"
+
+while read ip; do
+    if [ -z "$ip" ]; then
+        continue
+    fi
+
+    echo -n "Probando $ip ... "
+
+    # Enviar ping con el patrón mágico ffffffff
+    if ping -c 1 -p ffffffff -W 2 $ip > /dev/null 2>&1; then
+        echo "¡¡¡ ALERTA !!!"
+        echo ">>> $ip RESPONDE al paquete 0xFFFFFFFF <<<"
+        echo ">>> POSIBLE BPFdoor DETECTADO <<<"
+        echo "$ip - POSIBLE BPFdoor" >> bpfdoor_detectados.txt
+    else
+        echo "Sin respuesta"
+    fi
+done < $FICHERO
+
+echo ""
